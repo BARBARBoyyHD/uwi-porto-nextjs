27a8df148c000b36d74c0edd4ca69143e33e7491
BARBARBoyyHD <helyxrulk@gmail.com>
2025-11-08 16:58:24 +0700

    fix : si delete lagi delete lagi

--- src/app/api/v1/admin/my-services/delete/[id]/route.ts
+++ src/app/api/v1/admin/my-services/delete/[id]/route.ts
@@ -1,16 +1,11 @@
 import { deleteHandler } from "@/lib/api/deleteHandler";
 import { errorResponse } from "@/utils/response";
 import { NextRequest } from "next/server";
-
-interface Params {
-  params: {
-    id: string;
-  };
-}
+import type { Params } from "@/types/params";
 
 export async function DELETE(_request: NextRequest, { params }: Params) {
   try {
-    const { id } = params;
+    const { id } = await params;
     return await deleteHandler({ id, table: "my_services" });
   } catch (error: unknown) {
     if (error instanceof Error) {
